2e0e3cd3bb560d19288d663b27a2d93719c3d023
Suboor Khan <suboork100@gmail.com>
2023-10-02 04:32:31 +0530

    brdcrmb

--- src/components/Ellipsis.jsx
+++ src/components/Ellipsis.jsx
@@ -4,6 +4,7 @@ const Ellipsis = ({ text = '', className = '' }) => {
     const [isCollapsed, setIsCollapsed] = useState(true);
 
     const expand = () => setIsCollapsed(false);
+    const collapse = () => setIsCollapsed(true);
 
     return (
         <p className={`${className} ${isCollapsed ? 'ellipsis' : ''}`} style={{ position: 'relative', fontSize: '17px' }}>
@@ -11,6 +12,12 @@ const Ellipsis = ({ text = '', className = '' }) => {
             {isCollapsed && (
                 <a onClick={expand} className="readMoreBtn">
                     ...Read More
+                </a>
+            )}
+
+            {!isCollapsed && (
+                <a onClick={collapse} className='text-prim'>
+                    ...Read Less
                 </a>
             )}
         </p>
