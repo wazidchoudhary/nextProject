62404896af5044b02f568ad2cf2c30f9a7888924
Suboor Khan <suboork100@gmail.com>
2023-10-07 17:59:02 +0530

    mobile nav

---
+++ src/pages/emptyCart/index.js
@@ -0,0 +1,24 @@
+import BreadCrumb from '@/seo/BreadCrumb';
+import Container from '@/components/Layout/Container';
+import { useEffect, useState } from 'react';
+import { EmptyProductSection } from '@/components/EmptyProductSection';
+import { CartHelper } from '@/lib/cart';
+
+
+export default function ({}) {
+    const title = 'Empty Cart';
+    const breadCrumbItems = [{ url: '/', name: 'Home' }, { name: title }];
+
+    useEffect(() => {
+        CartHelper.emptyCart();
+    }, []);
+
+    return (
+        <>
+            <BreadCrumb items={breadCrumbItems} text={title} />
+            <Container>
+                {<EmptyProductSection containerHeight="auto" description={'Empty Cart'} />}
+            </Container>
+        </>
+    );
+}
